54f3eb635fc6e8175450fba602295ee54096ddc8
Romain Gilliotte <rgilliotte@gmail.com>
2019-07-28 22:00:13 +0200

    Make developing in docker easier

--- docker/build.sh
+++
@@ -1,8 +0,0 @@
-
-# This script builds and publish the docker images that are needed to run monitool.
-
-docker image build -t medecinsdumonde/monitool-api:latest ../api
-docker image build -t medecinsdumonde/monitool-frontend:latest ../frontend
-
-docker image push medecinsdumonde/monitool-api
-docker image push medecinsdumonde/monitool-frontend
