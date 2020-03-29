03c000cec417d03aed0d127a7bac84fd8b3fc293
Romain Gilliotte <rgilliotte@gmail.com>
2020-03-29 13:47:18 +0200

    Move code to olap-in-memory

--- api/src/tasks/reporting/query-runner.js
+++ api/src/tasks/reporting/query-runner.js
@@ -1,39 +1,11 @@
 const _ = require('lodash');
-const { GenericDimension } = require('olap-in-memory');
 const { getIndicatorCube } = require('./loader/indicator');
 
 async function executeQuery(projectId, output, query) {
     const cube = await loadQueryCube(projectId, query);
 
     // Format response.
-    if (output == 'report') {
-        if (query.aggregate.length === 0) {
-            return cube.getNestedObject('main')
-        }
-        else {
-            const report = {};
-
-            for (let j = 0; j < 2 ** cube.dimensions.length; ++j) {
-                const dimensionIds = cube.dimensionIds.slice();
-
-                let subCube = cube;
-                for (let i = 0; i < cube.dimensions.length; ++i) {
-                    const include = j & (1 << i);
-                    if (include !== 0) {
-                        dimensionIds[i] = `total_${i}`;
-                        subCube = subCube
-                            .removeDimension(cube.dimensions[i].id)
-                            .addDimension(new GenericDimension(`total_${i}`, 'none', ['_total']))
-                    }
-                }
-
-                const subReport = subCube.reorderDimensions(dimensionIds).getNestedObject('main');
-                _.merge(report, subReport);
-            }
-
-            return report;
-        }
-    }
+    if (output == 'report') return cube.getNestedObject('main', true);
     else if (output == 'flatArray') return cube.getFlatArray('main');
     else if (output == 'nestedArray') return cube.getNestedArray('main');
     else return cube.getNestedObject('main');
