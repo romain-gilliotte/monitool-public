7e94e40c0e07b48e141947a80346c7666c9c1992
Romain Gilliotte <rgilliotte@gmail.com>
2020-04-10 10:51:54 +0200

    start thinking about project homepage

--- frontend/src/components/pages/input-home/project-input-home.js
+++ frontend/src/components/pages/input-home/project-input-home.js
@@ -13,7 +13,8 @@ module.config($stateProvider => {
 
 module.component(__componentName, {
 	bindings: {
-		project: '<'
+		project: '<',
+		invitations: '<'
 	},
 	template: require(__templatePath),
 	controller: class ProjectInputHomeController {
@@ -23,8 +24,41 @@ module.component(__componentName, {
 		}
 
 		async $onChanges(changes) {
-			this.status = {};
+			this.users = [
+				{
+					fullname: 'Romain Gilliotte',
+					email: 'rgilliotte@gmail.com',
+					avatar: 'https://lh3.googleusercontent.com/a-/AOh14GgY2GNGIEUwXqL9_5eJcwNWX-LNW6acp8_75NGD',
+					lastSeen: new Date()
+				}, {
+					fullname: 'Romain Gilliotte',
+					email: 'eloims@gmail.com',
+					avatar: 'https://lh4.googleusercontent.com/-01ULdD2txAU/AAAAAAAAAAI/AAAAAAAAAAA/AAKWJJMBmuYnK1aEDdp6Ymj5BOOwQPUiLA/photo.jpg',
+					lastSeen: new Date()
+				}, {
+					fullname: 'Romain Gilliotte',
+					email: 'rgilliotte2@gmail.com',
+					avatar: 'https://lh3.googleusercontent.com/a-/AOh14GgY2GNGIEUwXqL9_5eJcwNWX-LNW6acp8_75NGD',
+					lastSeen: new Date()
+				}, {
+					fullname: 'Romain Gilliotte',
+					email: 'eloims2@gmail.com',
+					avatar: 'https://lh4.googleusercontent.com/-01ULdD2txAU/AAAAAAAAAAI/AAAAAAAAAAA/AAKWJJMBmuYnK1aEDdp6Ymj5BOOwQPUiLA/photo.jpg',
+					lastSeen: new Date()
+				}, {
+					fullname: 'Romain Gilliotte',
+					email: 'rgilliotte3@gmail.com',
+					avatar: 'https://lh3.googleusercontent.com/a-/AOh14GgY2GNGIEUwXqL9_5eJcwNWX-LNW6acp8_75NGD',
+					lastSeen: new Date()
+				}, {
+					fullname: 'Romain Gilliotte',
+					email: 'eloims4@gmail.com',
+					avatar: 'https://lh4.googleusercontent.com/-01ULdD2txAU/AAAAAAAAAAI/AAAAAAAAAAA/AAKWJJMBmuYnK1aEDdp6Ymj5BOOwQPUiLA/photo.jpg',
+					lastSeen: new Date()
+				},
+			];
 
+			this.status = {};
 			this.project.forms.forEach(async form => {
 				this.status[form.id] = await Input.fetchFormShortStatus(this.project, form.id);
 				this.$scope.$apply();
