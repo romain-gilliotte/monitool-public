6e4129f22f3b22f0e41453209763607fa8d95f68
Romain Gilliotte <rgilliotte@gmail.com>
2020-04-11 12:44:59 +0200

    clean up

--- api/src/middlewares/last-seen.js
+++ api/src/middlewares/last-seen.js
@@ -3,17 +3,39 @@ const TimeSlot = require('timeslot-dag');
 const demoProject = require('../../data/demo-project/project.json');
 const demoInputs = require('../../data/demo-project/inputs.json');
 
-function offsetTimeSlot(slot, months) {
-    if (slot) {
-        const ts = new TimeSlot(slot);
-        const time = new Date(.5 * (ts.firstDate.getTime() + ts.lastDate.getTime()));
-        time.setUTCMonth(time.getUTCMonth() + months);
-        return TimeSlot.fromDate(time, ts.periodicity).value;
+module.exports = async (ctx, next) => {
+    // Update user profile in database.
+    const result = await database.collection('user').replaceOne(
+        { _id: ctx.state.profile.email },
+        {
+            name: ctx.state.profile.name,
+            picture: ctx.state.profile.picture,
+            lastSeen: new Date()
+        },
+        { upsert: true }
+    );
+
+    // This is a new user, create the default project.
+    if (result.upsertedCount > 0) {
+        const dateFrom = new Date(demoProject.start + 'T00:00:00Z');
+        const dateTo = new Date();
+        const monthOffset =
+            dateTo.getUTCMonth() - dateFrom.getUTCMonth()
+            + 12 * (dateTo.getUTCFullYear() - dateFrom.getUTCFullYear())
+            - 15; // we have 16 months of data in the demo project
+
+        const project = offsetProject({ owner: ctx.state.profile.email, ...demoProject }, monthOffset);
+        await database.collection('project').insertOne(project);
+
+        const sequence = { projectIds: [project._id] };
+        await database.collection('input_seq').insertOne(sequence);
+
+        const inputs = demoInputs.map(input => offsetInput({ sequenceId: sequence._id, ...input }, monthOffset));
+        await database.collection('input').insertMany(inputs);
     }
-    else {
-        return slot;
-    }
-}
+
+    await next();
+};
 
 function offsetProject(project, offset) {
     return {
@@ -42,37 +64,14 @@ function offsetInput(input, offset) {
     }
 }
 
-module.exports = async (ctx, next) => {
-    const result = await database.collection('user').replaceOne(
-        { _id: ctx.state.profile.email },
-        {
-            name: ctx.state.profile.name,
-            picture: ctx.state.profile.picture,
-            lastSeen: new Date()
-        },
-        { upsert: true }
-    );
-
-    // This is a new user, create the default project.
-    if (result.upsertedCount > 0) {
-        // Compute how much we need to offset the demo project.
-        const dateFrom = new Date(demoProject.start + 'T00:00:00Z');
-        const dateTo = new Date();
-
-        let monthOffset =
-            dateTo.getUTCMonth() - dateFrom.getUTCMonth()
-            + 12 * (dateTo.getUTCFullYear() - dateFrom.getUTCFullYear())
-            - 15; // we have 16 months of data in the demo project
-
-        const project = offsetProject({ owner: ctx.state.profile.email, ...demoProject }, monthOffset);
-        await database.collection('project').insertOne(project);
-
-        const sequence = { projectIds: [project._id] };
-        await database.collection('input_seq').insertOne(sequence);
-
-        const inputs = demoInputs.map(input => offsetInput({ sequenceId: sequence._id, ...input }, monthOffset));
-        await database.collection('input').insertMany(inputs);
+function offsetTimeSlot(slot, months) {
+    if (slot) {
+        const ts = new TimeSlot(slot);
+        const time = new Date(.5 * (ts.firstDate.getTime() + ts.lastDate.getTime()));
+        time.setUTCMonth(time.getUTCMonth() + months);
+        return TimeSlot.fromDate(time, ts.periodicity).value;
     }
-
-    await next();
-};
+    else {
+        return slot;
+    }
+}
