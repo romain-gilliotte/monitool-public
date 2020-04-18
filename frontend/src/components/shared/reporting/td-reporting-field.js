542d23f58aeea509e290932726762feda9c05c11
Romain Gilliotte <rgilliotte@gmail.com>
2020-04-18 19:23:04 +0200

    Clean up td-reporting-field, and differenciate div by zero

--- frontend/src/components/shared/reporting/td-reporting-field.js
+++ frontend/src/components/shared/reporting/td-reporting-field.js
@@ -9,8 +9,6 @@ module.directive(__componentName, function () {
 		scope: {}, // Isolate
 
 		bindToController: {
-			interpolated: '<',
-			incomplete: '<',
 			value: '<',
 			baseline: '<',
 			target: '<',
@@ -18,73 +16,94 @@ module.directive(__componentName, function () {
 			unit: '<'
 		},
 
-		template: '<span ng-bind-html="$ctrl.display"></span><i ng-if="$ctrl.logo" class="fa" ng-class="$ctrl.logo"></i>',
-
 		controller: class ReportingFieldController {
 
-			constructor($element, $filter, $sce) {
-				this.$sce = $sce;
+			constructor($element) {
 				this.$element = $element;
 			}
 
 			$onChanges(changes) {
-				// Reset bgcolor
-				this.$element.css('background-color', '');
-				this.display = '';
-				this.logo = null;
+				if (this.value) {
+					let { v: value, c: complete, r: raw } = this.value;
 
-				if (this.value === undefined) {
+					const text = this._getText(value, complete, raw)
+					this.$element.html(text);
+
+					const color = this._getColor(value, complete, raw);
+					this.$element.css('background-color', color);
+
+					const title = this._getTitle(value, complete, raw);
+					if (title) this.$element.attr('title', title);
+					else this.$element.removeAttr('title');
+
+					const fontStyle = complete ? 'inherit' : 'italic';
+					this.$element.css('font-style', fontStyle);
+				}
+				else {
+					this.$element.html('');
 					this.$element.css('background-color', '#eee');
+					this.$element.removeAttr('title');
+					this.$element.css('font-style', 'inherit');
 				}
-				else if (this.value === null) {
-					// Can either be because the value is NaN, or because the data entry is missing.
-					// fixme: differenciate those.
-					this.logo = 'fa-question-circle-o';
-					this.$element.attr('title', 'This value cannot be computed because the data entry was not done.');
-				}
-				else if (typeof this.value === "number") {
-					// Make color
-					if (this.colorize && this.baseline !== null && this.target !== null) {
-						let progress = (this.value - this.baseline) / (this.target - this.baseline);
-						progress = Math.max(0, progress);
-						progress = Math.min(1, progress);
+			}
 
-						this.$element.css('background-color', 'hsl(' + progress * 120 + ', 100%, 75%)');
-					}
-
-					this.$element.css('font-style', this.incomplete ? 'italic' : 'inherit');
-
-					if (this.interpolated && this.incomplete)
-						this.$element.attr('title', 'This value was computed from data which is interpolated and incomplete.');
-					else if (this.interpolated)
-						this.$element.attr('title', 'This value was computed from data which is interpolated.');
-					else if (this.incomplete)
-						this.$element.attr('title', 'This value was computed from data which is incomplete.');
+			_getText(value, complete, raw) {
+				if (value === null) {
+					if (complete)
+						return '<i class="fa fa-exclamation"></i>';
 					else
-						this.$element.removeAttr('title');
-
+						return '<i class="fa fa-question-circle-o"></i>';
+				}
+				else if (typeof value === 'number') {
 					// Split value by thousands
-					let value = Math.round(this.value).toString();
-					if (value !== 'Infinity') {
-						this.display = '';
-						while (value.length !== 0) {
-							this.display = '.' + value.substr(-3) + this.display;
-							value = value.substr(0, value.length - 3);
+					let text = Math.round(value).toString();
+					if (text !== 'Infinity') {
+						let splitted = '';
+						while (text.length > 0) {
+							splitted = '.' + text.substr(-3) + splitted;
+							text = text.substr(0, text.length - 3);
 						}
-						this.display = this.display.substring(1);
-					}
-					else
-						this.display = value;
-
-					if (this.interpolated) {
-						this.display = '≈&nbsp;' + this.display;
+						text = splitted.substring(1);
 					}
 
-					// Add unit
+					if (!raw)
+						text = '≈&nbsp;' + text;
+
 					if (this.unit)
-						this.display = this.display + this.unit;
+						text = text + this.unit;
 
-					this.display = this.$sce.trustAsHtml(this.display)
+					return text;
+				}
+
+				return '';
+			}
+
+			_getColor(value, complete, raw) {
+				if (typeof value === 'number' && this.colorize && this.baseline !== null && this.target !== null) {
+					let progress = (value - this.baseline) / (this.target - this.baseline);
+					progress = Math.max(0, progress);
+					progress = Math.min(1, progress);
+
+					return 'hsl(' + progress * 120 + ', 100%, 75%)';
+				}
+
+				return '';
+			}
+
+			_getTitle(value, complete, raw) {
+				if (value === null) {
+					if (complete)
+						return 'Division by zero';
+					else
+						return 'This value cannot be computed because the data entry was not done.';
+				}
+				else if (typeof value === 'number') {
+					if (!raw && !complete)
+						return 'This value was computed from data which is interpolated and incomplete.';
+					else if (!raw)
+						return 'This value was computed from data which is interpolated.';
+					else if (!complete)
+						return 'This value was computed from data which is incomplete.';
 				}
 			}
 		}
