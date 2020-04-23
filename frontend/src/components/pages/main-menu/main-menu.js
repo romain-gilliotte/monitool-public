f20396ff4478f09308bbab80ac73654a0adf5309
Romain Gilliotte <rgilliotte@gmail.com>
2020-04-23 10:26:28 +0200

    Minor css clean up. Still a lot to do

--- frontend/src/components/pages/main-menu/main-menu.js
+++ frontend/src/components/pages/main-menu/main-menu.js
@@ -1,39 +1,39 @@
-import uiRouter from '@uirouter/angularjs';
-import angular from 'angular';
-import axios from 'axios';
-import Project from '../../../models/project';
+import uiRouter from '@uirouter/angularjs'
+import angular from 'angular'
+import axios from 'axios'
+import Project from '../../../models/project'
+require(__scssPath)
 
-const module = angular.module(__moduleName, [uiRouter]);
+const module = angular.module(__moduleName, [uiRouter])
 
-module.config($stateProvider => {
+module.config(($stateProvider) => {
     $stateProvider.state('main', {
         abstract: true,
         component: __componentName,
         resolve: {
             projects: () => Project.fetchAll(),
             invitations: async () => {
-                const response = await axios.get(`/invitation`);
-                return response.data;
-            }
-        }
-    });
-
-});
+                const response = await axios.get(`/invitation`)
+                return response.data
+            },
+        },
+    })
+})
 
 module.component(__componentName, {
     bindings: {
         projects: '<',
-        invitations: '<'
+        invitations: '<',
     },
     template: require(__templatePath),
 
     controller: class {
         logout() {
             window.auth0.logout({
-                returnTo: window.location.origin
-            });
+                returnTo: window.location.origin,
+            })
         }
-    }
-});
+    },
+})
 
-export default module.name;
+export default module.name
