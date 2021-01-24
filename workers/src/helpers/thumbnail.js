57a7f0dfb58975cf0e3f45055a0d21313f53810d
Romain Gilliotte <rgilliotte@gmail.com>
2021-01-24 23:22:56 +0100

    Remove unoconv for thumbnails

--- workers/src/helpers/thumbnail.js
+++ workers/src/helpers/thumbnail.js
@@ -1,11 +1,11 @@
-const axios = require('axios');
-const FormData = require('form-data');
-const fs = require('fs');
+const childproc = require('child_process');
+const fs = require('fs/promises');
 const gm = require('gm');
 const util = require('util');
-const config = require('../config');
+const os = require('os');
+const path = require('path');
 
-const unoconvMimes = {
+const libreofficeMimes = {
     'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': 'xlsx',
 };
 
@@ -20,8 +20,6 @@ const otherTypes = {
     'application/zip': 'zip',
 };
 
-const defaultThumbnail = fs.readFileSync('data/thumbnail.png');
-
 /**
  * Make a 300x200 thumbnail
  *
@@ -29,41 +27,64 @@ const defaultThumbnail = fs.readFileSync('data/thumbnail.png');
  * @param {string} mimeType
  */
 async function generateThumbnail(buffer, mimeType) {
-    const ext = unoconvMimes[mimeType] || gmMimes[mimeType] || otherTypes[mimeType];
-
-    if (config.unoconv.uri && unoconvMimes[mimeType] && !gmMimes[mimeType]) {
-        const form = new FormData();
-        form.append('file', buffer, `file.${unoconvMimes[mimeType]}`);
-
-        const response = await axios.post(config.unoconv.uri, form, {
-            responseType: 'arraybuffer',
-            headers: form.getHeaders(),
-        });
-
-        buffer = response.data;
-        mimeType = 'application/pdf';
-    }
+    const ext = libreofficeMimes[mimeType] || gmMimes[mimeType] || otherTypes[mimeType];
+    const tmpDir = await fs.mkdtemp(path.join(os.tmpdir(), 'monitool-thumbnail-'));
 
     try {
-        if (!gmMimes[mimeType]) {
+        if (libreofficeMimes[mimeType]) {
+            const filePath = path.join(tmpDir, `file`);
+            let numTries = 10;
+
+            await fs.writeFile(`${filePath}.xlsx`, buffer);
+            while (numTries > 0) {
+                numTries--;
+
+                try {
+                    await new Promise((resolve, reject) => {
+                        childproc.exec(
+                            `libreoffice --headless --convert-to pdf ${filePath}.xlsx`,
+                            { cwd: tmpDir },
+                            (error, stdout) => {
+                                if (error) reject(error);
+                                else resolve(stdout);
+                            }
+                        );
+                    });
+
+                    buffer = await fs.readFile(`${filePath}.pdf`);
+                    mimeType = 'application/pdf';
+                    break;
+                } catch (e) {
+                    // For some reason, libreoffice sometime crashes when running in parallel.
+                    // => Just retry in a couple seconds.
+                    await new Promise(resolve => setTimeout(resolve, Math.random() * 1000));
+                    continue;
+                }
+            }
+        }
+
+        if (gmMimes[mimeType]) {
+            let image = gm(buffer, `file.${gmMimes[mimeType]}`);
+            const { width, height } = await util.promisify(image.size.bind(image))();
+
+            const ratio = width / height;
+            if (ratio < 1.5) {
+                image = image.crop(width, width / 1.5);
+            } else {
+                image = image.crop(height * 1.5, height);
+            }
+
+            image = image.resize(300, 200);
+
+            buffer = await util.promisify(image.toBuffer.bind(image))('PNG');
+            mimeType = 'image/png';
+        } else {
             throw new Error('Invalid type');
         }
-
-        let image = gm(buffer, `file.${gmMimes[mimeType]}`);
-        const { width, height } = await util.promisify(image.size.bind(image))();
-
-        const ratio = width / height;
-        if (ratio < 1.5) {
-            image = image.crop(width, width / 1.5);
-        } else {
-            image = image.crop(height * 1.5, height);
-        }
-
-        image = image.resize(300, 200);
-
-        buffer = await util.promisify(image.toBuffer.bind(image))('PNG');
-        mimeType = 'image/png';
     } catch (e) {
+        console.log(e);
+
+        const defaultThumbnail = await fs.readFile('data/thumbnail.png');
         const image = gm(defaultThumbnail, 'file.png')
             .fill('#000000')
             .font('Arial-Bold', 30)
@@ -71,6 +92,8 @@ async function generateThumbnail(buffer, mimeType) {
 
         buffer = await util.promisify(image.toBuffer.bind(image))('PNG');
         mimeType = 'image/png';
+    } finally {
+        await fs.rmdir(tmpDir, { recursive: true });
     }
 
     return buffer;
